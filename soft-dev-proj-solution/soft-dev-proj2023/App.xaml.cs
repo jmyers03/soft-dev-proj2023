7f4be88bf427033921bcf8c0c450fa00d0bfd420
Josh Dellamuth <jddellamut@eagles.usi.edu>
2023-10-12 20:31:23 -0500

    added default Maui app to develop from

---
+++ soft-dev-proj-solution/soft-dev-proj2023/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace soft_dev_proj2023;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ soft-dev-proj-solution/soft-dev-proj2023/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace soft_dev_proj2023;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
