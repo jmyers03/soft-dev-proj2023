99b37bb3761f659743fcb85ebead53102b0637bd
jmyers003 <114010428+jmyers003@users.noreply.github.com>
2023-10-19 18:02:24 -0500

    updated Login.xaml Login.xaml.cs App.xaml #27

--- soft-dev-proj-solution/soft-dev-proj2023/LoginPage.xaml.cs
+++ soft-dev-proj-solution/soft-dev-proj2023/LoginPage.xaml.cs
@@ -1,24 +1,14 @@
-﻿namespace soft_dev_proj2023;
+﻿
+namespace soft_dev_proj2023;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
