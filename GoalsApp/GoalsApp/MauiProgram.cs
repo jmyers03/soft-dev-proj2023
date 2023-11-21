1f5d2f40475bfe12d20e6184f3dc0b0d63943076
Josh Dellamuth <joshdellamuth@gmail.com>
2023-11-21 01:54:05 -0600

    Registered 5 new fonts and files to register more if needed

--- GoalsApp/GoalsApp/MauiProgram.cs
+++ GoalsApp/GoalsApp/MauiProgram.cs
@@ -14,7 +14,14 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 
-			});
+                //Newly added fonts: 
+                fonts.AddFont("Montserrat-Regular.ttf", "Montserrat");
+                fonts.AddFont("Montserrat-Bold.ttf", "MontserratBold");
+                fonts.AddFont("Montserrat-ExtraBold.ttf", "MontserratExtraBold"); //pretty bold, maybe too bold 
+                fonts.AddFont("Montserrat-Light.ttf", "MontserratLight");
+                fonts.AddFont("Montserrat-Italic.ttf", "MontserratItalic");
+
+            });
 
 
         builder.Services.AddTransient<LoginPage>();
