00302baf26e15c768836bd8b3b20e31814b79e27
Josh Dellamuth <joshdellamuth@gmail.com>
2023-11-22 05:09:35 -0600

    Edited code to show Dashboard page

--- GoalsApp/GoalsApp/AppShell.xaml.cs
+++ GoalsApp/GoalsApp/AppShell.xaml.cs
@@ -8,8 +8,9 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 
-		//ADD NEW PAGE ROUTES HERE
-		Routing.RegisterRoute(nameof(PasswordRecovery), typeof(PasswordRecovery));
+        //ADD NEW PAGE ROUTES HERE
+        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        Routing.RegisterRoute(nameof(PasswordRecovery), typeof(PasswordRecovery));
 		Routing.RegisterRoute(nameof(SignUpPage), typeof(SignUpPage));
         Routing.RegisterRoute(nameof(TaskPage), typeof(TaskPage));
         Routing.RegisterRoute(nameof(CalendarPage), typeof(CalendarPage));
