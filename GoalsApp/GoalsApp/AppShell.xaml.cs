15066bac13a75a4337640bdb4e1a6362a38f8d96
Josh Dellamuth <joshdellamuth@gmail.com>
2023-11-22 02:02:17 -0600

    Added UI Experiments page and will add to flyout

--- GoalsApp/GoalsApp/AppShell.xaml.cs
+++ GoalsApp/GoalsApp/AppShell.xaml.cs
@@ -8,9 +8,11 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 
+		//ADD NEW PAGE ROUTES HERE
 		Routing.RegisterRoute(nameof(PasswordRecovery), typeof(PasswordRecovery));
 		Routing.RegisterRoute(nameof(RegistrationPage), typeof(RegistrationPage));
         Routing.RegisterRoute(nameof(TaskPage), typeof(TaskPage));
         Routing.RegisterRoute(nameof(CalendarPage), typeof(CalendarPage));
+        Routing.RegisterRoute(nameof(UIExperiments), typeof(UIExperiments));
     }
 }
