c6e442b9c5dee2ec56098649f6f5c87d76a34ce5
Josh Dellamuth <joshdellamuth@gmail.com>
2023-11-27 00:53:04 -0600

    Fixed LoginPage navigation issue

--- GoalsApp/GoalsApp/AppShell.xaml.cs
+++ GoalsApp/GoalsApp/AppShell.xaml.cs
@@ -8,13 +8,13 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 
-        //ADD NEW PAGE ROUTES HERE
-        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+		//ADD NEW PAGE ROUTES HERE
+		Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        Routing.RegisterRoute(nameof(Dashboard), typeof(Dashboard));
         Routing.RegisterRoute(nameof(PasswordRecovery), typeof(PasswordRecovery));
 		Routing.RegisterRoute(nameof(SignUpPage), typeof(SignUpPage));
-        Routing.RegisterRoute(nameof(TaskPage), typeof(TaskPage));
-        Routing.RegisterRoute(nameof(CalendarPage), typeof(CalendarPage));
-        Routing.RegisterRoute(nameof(UIExperiments), typeof(UIExperiments));
-        Routing.RegisterRoute(nameof(Dashboard), typeof(Dashboard));
-    }
+		Routing.RegisterRoute(nameof(TaskPage), typeof(TaskPage));
+		Routing.RegisterRoute(nameof(CalendarPage), typeof(CalendarPage));
+		Routing.RegisterRoute(nameof(UIExperiments), typeof(UIExperiments));
+	}
 }
