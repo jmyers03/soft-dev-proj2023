0c17c7d4ad5b083750ec5b639e9a89aba63d14f2
jmyers003 <114010428+jmyers003@users.noreply.github.com>
2023-12-02 20:34:58 -0600

    Added RestSharp databse API test file and test api call

--- GoalsApp/GoalsApp/FirebaseAPI/RestSharpTest.cs
+++ GoalsApp/GoalsApp/FirebaseAPI/RestSharpTest.cs
@@ -8,7 +8,7 @@ public class RestSharpClass
     public async void RestSharpConstructor()
     {
 
- 
+
         var options = new RestClientOptions("https://api.twitter.com/1.1")
         {
             Authenticator = new HttpBasicAuthenticator("username", "password")
@@ -19,8 +19,8 @@ public class RestSharpClass
         var request = new RestRequest("statuses/home_timeline.json");
         // The cancellation token comes from the caller. You can still make a call without it.
 
-        var response = await client.GetAsync(request, cancellationToken);
-        
+        var response = await client.GetAsync(request); //cancellationToken);
+
     }
 
 }
